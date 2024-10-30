671338b0ebf894e7d3a67285a240764e605bfde4
azamat09912 <maratazamat09@gmail.com>
2024-10-31 00:28:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+
+var name= 'azamat'
+
+var azamat;
+
+console.log("azamat mani: " + azamat);
+
+console.log(typeof name); //string
+
+var age= 14  
+
+console.log("age mani: " + 14);
+
+console.log(typeof age); //number
+
+ var age= toString("15")
+
+ 
+console.log(typeof name); //string
+
+
+var durys = true;
+var qate = false;
+
+var san1 = 10
+var san2 = 15
+
+natizhe = san1 + san2 // 10 + 15
+console.log("Азаматың нәтижесі:  " + natizhe);
+
+
+if(10 + 15 == 25){
+    console.log(durys);
+}
+else{
+    console.log(qate);    
+}
+console.log(typeof Boolean); //Buoleon
+
+
+
+
